d4baeb26a9e573e40cc24db838741f6d4475005d
Anton <122563428+anton3029941@users.noreply.github.com>
2023-09-04 22:02:59 +0600

    Add files via upload

---
+++ Password Generator.py
@@ -0,0 +1,15 @@
+import random
+symbols = "qwertyuiop1234567890asdfghjklzxcvbnm._QWERTYUIOPASDFGHJKLZXCVBNM"
+symbols = list(symbols)
+false = True
+while false:
+    length = input("Длина пароля: ")
+    try:
+        int(length)
+        break
+    except:
+        print("Try again")
+password = ""
+for i in range(int(length)):
+    password += symbols[random.randint(0, len(symbols))]
+print(password)
